d93359de2fdb2e0e494b4135c2c475a2bc25d7f7
Andreas Klein <mail@andreasklein.org>
2016-02-26 21:34:01 +0100

    Try widget rendering (server)

--- js/main.js
+++ js/main.js
@@ -2,54 +2,54 @@
 
 var $ = require('jquery');
 
-var Avlb = function () {
-    this.$el = $('[avlb-root]');
-    this.$regForm = this.$el.find('[avlb-register-user]');
+// var Avlb = function () {
+//     this.$el = $('[avlb-root]');
+//     this.$regForm = this.$el.find('[avlb-register-user]');
 
-    this.registerUser();
-};
+//     this.registerUser();
+// };
 
-Avlb.prototype.registerUser = function () {
-    var $username = this.$regForm.find('#username'),
-        $useremail = this.$regForm.find('#useremail'),
-        $formMessage = this.$regForm.find('[avlb-register-message]');
+// Avlb.prototype.registerUser = function () {
+//     var $username = this.$regForm.find('#username'),
+//         $useremail = this.$regForm.find('#useremail'),
+//         $formMessage = this.$regForm.find('[avlb-register-message]');
 
-    this.$regForm.on('submit', function (e) {
-        e.preventDefault();
+//     this.$regForm.on('submit', function (e) {
+//         e.preventDefault();
 
-        var data = {
-            user: {
-                email: $useremail.val(),
-                name: $username.val()
-            }
-        };
+//         var data = {
+//             user: {
+//                 email: $useremail.val(),
+//                 name: $username.val()
+//             }
+//         };
 
-        $.ajax({
-            type: 'PUT',
-            url: '/api/user',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            contentType: 'application/json; charset=UTF-8',
-            error: function (data) {
-                $formMessage
-                    .text(data.responseJSON.message)
-                    .show();
+//         $.ajax({
+//             type: 'PUT',
+//             url: '/api/user',
+//             dataType: 'json',
+//             data: JSON.stringify(data),
+//             contentType: 'application/json; charset=UTF-8',
+//             error: function (data) {
+//                 $formMessage
+//                     .text(data.responseJSON.message)
+//                     .show();
 
-                console.log(data);
-            },
-            success: function (data) {
-                $formMessage
-                    .text(data.message)
-                    .show();
+//                 console.log(data);
+//             },
+//             success: function (data) {
+//                 $formMessage
+//                     .text(data.message)
+//                     .show();
 
-                console.log(data);
-            }
-        });
+//                 console.log(data);
+//             }
+//         });
 
-        return false;
-    });
-};
+//         return false;
+//     });
+// };
 
-$(function() {
-    new Avlb();
-});
+// $(function() {
+//     new Avlb();
+// });
